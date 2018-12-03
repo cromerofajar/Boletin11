2864ec2e52eb66a65d3dabbec56ce72d05802d91
cromerofajar <cromerofajar@nautilus7.danielcastelao.org>
2018-12-03 11:21:38 +0100

    Arreglado

--- boletin11/Instante.java
+++ boletin11/Instante.java
@@ -12,11 +12,16 @@ public class Instante {
     
     public void Programa(){
         long inicio, fin;
+        String fraseEscribir, fraseEscrita;
+        fraseEscribir="A documentación é todo aquel conxunto de manuais impresos ou en formato dixital que explique unha aplicación informática.";
         inicio=System.currentTimeMillis();
-        JOptionPane.showInputDialog(null, "Escriba la siguiente frase"+"\n A documentación é todo aquel conxunto de manuais impresos ou en formato dixital que explique unha aplicación informática.");
+        fraseEscrita=JOptionPane.showInputDialog(null, "Escriba la siguiente frase"+"\n A documentación é todo aquel conxunto de manuais impresos ou en formato dixital que explique unha aplicación informática.");
+        while(fraseEscribir.equals(fraseEscrita)!=true){
+        fraseEscrita=JOptionPane.showInputDialog(null, "Escriba la siguiente frase"+"\n A documentación é todo aquel conxunto de manuais impresos ou en formato dixital que explique unha aplicación informática."); 
+        }
         fin=System.currentTimeMillis();
         long total=fin-inicio;
-        JOptionPane.showMessageDialog(null,"Tardaches "+(total/1000)+" tempo en escribir a frase");
+        JOptionPane.showMessageDialog(null,"Tardaches "+(total/1000)+" segundos en escribir a frase");
         
     }
 }
